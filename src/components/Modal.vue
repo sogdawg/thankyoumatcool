f9e0faffe8f22732e669a4c2998b3a69ad3a68a2
sogdawg <andybendeluca@gmail.com>
2025-07-22 16:20:42 -0700

    please work!

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -7,10 +7,19 @@
             @click="close"
             class="absolute left-0 top-0 right-0 bottom-0 bg-black opacity-50 z-10"
         ></div>
-        <div class="relative z-20">
-            <article>
-                <slot></slot>
-            </article>
+        <div class="relative z-20 bg-white dark:bg-plain-gray rounded-lg p-5">
+            <!-- Named slot for header -->
+            <header v-if="$slots.header">
+                <slot name="header"></slot>
+            </header>
+            <!-- Named slot for body (default content area) -->
+            <section class="mt-4" v-if="$slots.body">
+                <slot name="body"></slot>
+            </section>
+            <!-- Named slot for footer -->
+            <footer class="mt-4 flex justify-end gap-2" v-if="$slots.footer">
+                <slot name="footer"></slot>
+            </footer>
         </div>
     </div>
 </template>
